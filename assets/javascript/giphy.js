99531c68280379b7fe1eceaefbb0866315edbe53
Casey K <caseykelleher@icloud.com>
2019-11-13 22:31:24 -0500

    addded html with containers to hold data

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -1,22 +1,41 @@
 
 
 
-var movieTitle = ["Goodfellas", "Godfather", "Scarface", "Serpico"];
+var movieTitle = ["Goodfellas", "The Godfather", "Scarface", "Serpico", "Gladiator", "The Perfect Storm", "Field of Dreams", "Apollo 13"]
 
 console.log(movieTitle);
 
-$()
+var movieBtn;
 
+var movieImage;
 
+function createButtons() {
 
+    $("#movie-btn-div").empty();
+
+    for( i =0; i < movieTitle.length; i++) {
+
+        var movieBtn = $("<button>");
+
+        movieBtn.text(movieTitle[i]);
+
+        movieBtn.attr("data-name", movieTitle[i]);
+
+        movieBtn.addClass("btn btn-primary p-2 mr-3 mb-2 movie-btn");
+
+        $("#movie-btn-div").append(movieBtn);
+
+    }
+}
+
+createButtons();
 
 $("buttons").on("click", function() {
 
-var movie = $(this).attr(".data-movie");
+var movie = $(this).attr("data-movie");
+console.log(movie);
 
-var apiKey = "rEQLIlplmzy2aXN8Sbpmkr6qJGH2cc4d";
-
-var queryUrl = "https://api.giphy.com/v1/gifs/search?q=" + movie + "&api_key=rEQLIlplmzy2aXN8Sbpmkr6qJGH2cc4d&limit=10";
+var queryUrl = "https://api.giphy.com/v1/gifs/search?q=" + movie + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
 
 $.ajax ({
 url: queryUrl,
@@ -29,23 +48,30 @@ method: "GET"
 
     console.log(response);
 
-    var response = response.data;
+    var results = response.data.title;
 
     for (var i = 0; i < results.length; index++) {
+        if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
+
+        var movieDiv = $("<div>");
+
+        var rating = results[i].rating;
+
+        var p = $("<p>").text("Rating: " + results[i].rating);
+
+        var movieImage = $("<img>");  
+
+        movieImage.attr("src", results[i].images.fixed_height.url);
+
+        movieDiv.append(p);
+        movieDiv.append(movieImage);
+
+        
+        $("#gifs-appear-here").prepend(movieDiv);
         
         
-    }
-
-
-
-
-
+        }
+    };
 });
-
-
-
-
-
-
 });
 
